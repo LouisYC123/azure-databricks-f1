f9aed07e40135161561b178cc7689ac58c200a66
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-08-08 18:15:36 +1000

    added includes py files

---
+++ workspace/Formula1/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1lgdl/raw"
+processed_folder_path = "/mnt/formula1lgdl/processed"
+presentation_folder_path = "/mnt/formula1lgdl/presentation"
+
+# COMMAND ----------
+
+
